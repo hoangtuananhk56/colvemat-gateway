109faddb8a14566ac21d2d0a2ce7a112b25536ac
Phillip <anhht@bfast.dev>
2023-07-09 15:49:31 +0700

    add microservice

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './event/event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handlerUserCreated - COMMUNICATIONS', data);
+    // TODO: Email the user...
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
 
-  const config = new DocumentBuilder()
-    .setTitle('Colvemat API')
-    .setDescription('Colvemat API')
-    .setVersion('1.0')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('Colvemat API')
+  //   .setDescription('Colvemat API')
+  //   .setVersion('1.0')
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
+  await app.listen();
 }
 bootstrap();
