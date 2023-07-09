9216900714c64ce98c844a804f310b01e055011a
Phillip <anhht@bfast.dev>
2023-07-09 15:57:54 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-// import { UserService } from './user/user.service';
 
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   data: 'db.sqlite',
-    // }),
-    ConfigModule.forRoot(),
-    UserModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-    },
-  );
+  const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Colvemat API')
-  //   .setDescription('Colvemat API')
-  //   .setVersion('1.0')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Colvemat API')
+    .setDescription('Colvemat API')
+    .setVersion('1.0')
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
-  await app.listen();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
 }
 bootstrap();
